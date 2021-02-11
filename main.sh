8154b65533b062d6fb967c9496f4ddc56f19bcda
Alvin Seville <alvinseville7cf@gmail.com>
2021-02-11 11:06:46 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+function outputText()
+{
+  local text=$1
+  local -i textLength=${#text}
+
+  local -i line="$(tput lines) / 2"
+  local -i col="$(tput cols) / 2 - $textLength / 2"
+
+  clear
+  echo -en "\e[$line;${col}H$text"
+}
+
+trap "outputText 'Hello world!'" SIGWINCH
+
+outputText 'Hello world!'
+while true
+do
+    :
+done
